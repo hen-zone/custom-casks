ec6579ad74ca4c22c1bff41fabea852a06397581
hen <hello@hen.zone>
2018-05-13 15:46:33 +0800

    Add undercast

---
+++ undercast.rb
@@ -0,0 +1,10 @@
+cask 'undercast' do
+  version :latest
+  sha256 '7bcf1369e5dbc153ca2a399b8c75bc797b9756ae9c9ac263e931a567256d0448'
+
+  url 'https://github.com/ImStuartJones/undercast/raw/master/releases/Undercast.app.zip'
+  name 'Undercast'
+  homepage 'https://github.com/ImStuartJones/undercast'
+
+  app 'Undercast.app'
+end
